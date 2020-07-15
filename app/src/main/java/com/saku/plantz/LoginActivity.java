acfa4af3676790a9b72b4f701737803cbe5f1752
tharukej <tharuke@hsenidoutsourcing.com>
2020-07-15 08:22:48 +0530

    login functionality completed

--- app/src/main/java/com/saku/plantz/LoginActivity.java
+++ app/src/main/java/com/saku/plantz/LoginActivity.java
@@ -1,29 +1,91 @@
 package com.saku.plantz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button gotoSignUp;
+    private TextInputEditText lgnEmail, lgnPassword;
+    private Button lgnButton, gotoSignUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        lgnEmail = findViewById(R.id.lgn_email);
+        lgnPassword = findViewById(R.id.lgn_password);
+        lgnButton = findViewById(R.id.lgnBtn);
         gotoSignUp = findViewById(R.id.gotoSignUp);
 
         gotoSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+
+        lgnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
             }
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        setUI(firebaseUser);
+    }
+
+    private void startSignIn() {
+        String email = lgnEmail.getText().toString();
+        String password = lgnPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Sign in problem", Toast.LENGTH_LONG).show();
+                    } else {
+                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                        setUI(firebaseUser);
+                    }
+
+                }
+
+            });
+        }
+    }
+
+    private void setUI(FirebaseUser firebaseUser) {
+        if (firebaseUser != null) {
+            this.finish();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+    }
+
 }
